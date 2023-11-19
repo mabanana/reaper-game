407d721759e55134bf0346f556ba2314dda8ca80
mabanana <aldwin.yu@hotmail.com>
2023-11-19 18:59:27 -0500

    Fix Frog Hitbox

--- main.gd
+++ main.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 func _ready():
-	Utils.saveGame()
-	Utils.loadGame()
-
+	#Utils.saveGame()
+	#Utils.loadGame()
+	pass
 
 func _on_button_2_pressed():
 	get_tree().quit() # Replace with function body.
