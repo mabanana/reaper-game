206c3f3cd5efd80248ce95229420e60ee9f7c4b6
mabanana <aldwin.yu@hotmail.com>
2023-11-19 19:37:24 -0500

    Add cherry spawning

---
+++ Collectables.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+var cherry = preload("res://cherry.tscn")
+
+
+func _on_timer_timeout():
+	print("spawn cherry")
+	var cherry_temp = cherry.instantiate()
+	var rng =RandomNumberGenerator.new()
+	var rand_int_x = rng.randi_range(100, 1150)
+	var rand_int_y = rng.randi_range(150, 250)
+	cherry_temp.position = Vector2(rand_int_x,rand_int_y)
+	add_child(cherry_temp)
