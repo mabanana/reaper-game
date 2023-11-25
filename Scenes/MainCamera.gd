b878d8de2ab5d9a776e19a8875f7762fde121165
mabanana <aldwin.yu@hotmail.com>
2023-11-24 21:13:25 -0500

    Expand World map

---
+++ Scenes/MainCamera.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#var scrolling_speed = 100
+#
+#func _process(delta):
+#	offset.x -= scrolling_speed * delta
--- Scenes/MenuButton.gd
+++ Scenes/MenuButton.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_pressed():
+	get_tree().quit()
