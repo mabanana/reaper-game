068fec177632a65be1525c76976f0e1a0a720a15
mabanana <aldwin.yu@hotmail.com>
2023-11-26 01:47:36 -0500

    Add Moving Platforms

---
+++ MovingBlock.gd
@@ -0,0 +1,12 @@
+extends AnimatableBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_parent().progress += 80 * delta
+	global_position = get_parent().global_position
