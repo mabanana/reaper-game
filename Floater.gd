0b315670dfe73d358e76b70d12239d6f66a0e1f3
mabanana <aldwin.yu@hotmail.com>
2023-12-13 04:42:03 -0500

    Update floater sprite

--- Floater.gd
+++ Floater.gd
@@ -5,13 +5,16 @@ var max_speed: int = 500
 var min_speed: int = 100
 var offset_x: int = 15
 var offset: Vector2 = Vector2(15,-10)
+@export var sprite_2d: AnimatedSprite2D
 
 func _physics_process(delta):
 	var displac = (Game.player_global_position + offset - global_position)
 	if displac.x > 0:
 		offset.x = -offset_x
+		sprite_2d.flip_h = false
 	elif displac.x < 0:
 		offset.x = offset_x
+		sprite_2d.flip_h = true
 
 	if Game.pet_acquired:
 		if chase:
