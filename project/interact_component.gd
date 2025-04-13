387a75c0a89d6735ae1159db9ea3afa3137ace83
mabanana <aldwin.yu@hotmail.com>
2025-04-13 19:50:37 -0400

    Add interaction component to reaper boss

---
+++ project/interact_component.gd
@@ -0,0 +1,32 @@
+@tool
+extends Node2D
+class_name InteractComponent
+
+@export var pos_offset: Vector2
+@export var label_text: String
+@export var parent: Node2D
+@onready var label: Label = $Label
+@onready var interact_range: Area2D = $Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+	interact_range.body_entered.connect(func(body):
+		if body is PlayerBody:
+			visible = true
+		)
+	interact_range.body_exited.connect(func(body):
+		if body is PlayerBody:
+			visible = false
+		)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = pos_offset
+	label.text = label_text
+
+func _on_body_entered(body):
+	if body is PlayerBody:
+		visible = true
