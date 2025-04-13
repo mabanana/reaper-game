40e3b05c27405de0af8f7db6ac630a36d3b5dcd3
mabanana <aldwin.yu@hotmail.com>
2025-04-13 18:35:09 -0400

    Update reset game function

--- project/Global/Game.gd
+++ project/Global/Game.gd
@@ -9,13 +9,13 @@ var unlocked_scare: bool = false
 var unlocked_float: bool = false
 var unlocked_fly: bool = false
 var scare_used: bool = false
+var pet_acquired: bool = false
+var boss_reaper_room: bool = false
+var boss_reaper_room_exit: bool = false
 
 var gems_collected: int = 0
 var player_global_position: Vector2
 var show_player_msg: bool = false
-var pet_acquired: bool = false
-var boss_reaper_room: bool = false
-var boss_reaper_room_exit: bool = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var play_animation_body: CharacterBody2D
 var dialogue_animation_playing: bool = false
@@ -37,6 +37,15 @@ func reset_state():
 	gems_collected = 0
 	show_player_msg = false
 	pet_acquired =false
+	jump_has_dmg = false
+	jump_has_dbl = false
+	unlocked_scare = false
+	unlocked_float = false
+	unlocked_fly = false
+	scare_used = false
+	pet_acquired = false
+	boss_reaper_room = false
+	boss_reaper_room_exit = false
 
 func test_func(text: String):
 	print(text)
