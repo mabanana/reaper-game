47519c3d7a2627345e8c5eaccc15c7c0d4ce5fa6
mabanana <aldwin.yu@hotmail.com>
2025-04-14 02:44:44 -0400

    Update Mob spawning to per spawner

--- project/Mobs.gd
+++ project/Mobs.gd
@@ -8,20 +8,18 @@ var spawn_interval = 15
 
 func _ready():
 	init_spawners()
-	_on_spawn_timer_timeout()
 	
 func init_spawners():
 	for spawner: Marker2D in spawners.get_children():
 		if spawner.name.to_lower().contains("frog"):
 			spawner_dict[spawner] = spawn_frog(spawner.global_position)
+			_on_spawn_timer_timeout(spawner)
 
-func _on_spawn_timer_timeout():
-	for spawner: Marker2D in spawner_dict.keys():
-		if spawner_dict[spawner] == null:
-			var new_frog = spawn_frog(spawner.global_position)
-			spawner_dict[spawner] = new_frog
-			break
-	get_tree().create_timer(spawn_interval).timeout.connect(_on_spawn_timer_timeout)
+func _on_spawn_timer_timeout(spawner: Marker2D):
+	if spawner_dict[spawner] == null:
+		var new_frog = spawn_frog(spawner.global_position)
+		spawner_dict[spawner] = new_frog
+	get_tree().create_timer(spawn_interval).timeout.connect(_on_spawn_timer_timeout.bind(spawner))
 
 func spawn_frog(pos: Vector2):
 	var frog_temp = frog.instantiate()
